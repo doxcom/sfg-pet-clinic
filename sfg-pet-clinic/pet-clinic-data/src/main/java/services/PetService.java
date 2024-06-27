daf6a4883156ac5b733029a24ba91185e6c002f8
doxcom <aldoperez1319@gmail.com>
2024-06-27 00:46:25 -0600

    refactor service interface to common base interface

--- sfg-pet-clinic/pet-clinic-data/src/main/java/services/PetService.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/services/PetService.java
@@ -1,14 +1,9 @@
 package services;
 
-import doxcom.springframework.sfgpetclinic.model.Owner;
 import doxcom.springframework.sfgpetclinic.model.Pet;
 
-import java.util.Set;
 
-public interface PetService {
 
-    Pet FindById(Long id);
-    Pet save(Pet pet);
+public interface PetService extends CrudService<Pet, Long> {
 
-    Set<Pet> findAll();
 }
