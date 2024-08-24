4f9dc4f35380142b11a44580cc817053f7655314
doxcom <aldoperez1319@gmail.com>
2024-08-23 18:53:29 -0600

    added PetTypeService closes #29

---
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/doxcom/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package doxcom.springframework.sfgpetclinic.services;
+
+import doxcom.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+    
+}
