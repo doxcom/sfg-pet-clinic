b5c3de384d9f1ab574dac486c06e1cf4147b362e
doxcom <aldoperez1319@gmail.com>
2024-08-25 16:59:11 -0600

    Added Specialties Map Impl. Closes #31

---
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/doxcom/springframework/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package doxcom.springframework.sfgpetclinic.services;
+
+import doxcom.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+}
