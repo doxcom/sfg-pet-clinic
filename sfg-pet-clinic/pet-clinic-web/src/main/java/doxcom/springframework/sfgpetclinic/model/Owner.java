a2ffcb4ea53c1dab39f9971f560a5edfdd033562
doxcom <aldoperez1319@gmail.com>
2024-09-02 16:33:08 -0600

    updated entities to JPA, closes #35 closes #36 closes #37

--- sfg-pet-clinic/pet-clinic-web/src/main/java/doxcom/springframework/sfgpetclinic/model/Owner.java
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/doxcom/springframework/sfgpetclinic/model/Owner.java
@@ -1,13 +1,25 @@
 package doxcom.springframework.sfgpetclinic.model;
 
+import jakarta.persistence.*;
+
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
 
+
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "telephone")
     private String telephone;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner") //if a delete an owner the pet also will be deleted
     private Set<Pet> pets = new HashSet<>();
 
     public String getCity() {
--- sfg-pet-clinic/pet-clinic-web/src/main/java/doxcom/springframework/sfgpetclinic/model/Vet.java
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/doxcom/springframework/sfgpetclinic/model/Vet.java
@@ -1,13 +1,22 @@
 package doxcom.springframework.sfgpetclinic.model;
 
+import jakarta.persistence.*;
+
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
     //lets initiate the values to avoid null pointer
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns =@JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
+
         return specialities;
     }
 
