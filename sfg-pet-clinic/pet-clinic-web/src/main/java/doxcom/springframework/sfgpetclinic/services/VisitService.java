f623e4468d09979b3f5477090951a5c489701d3d
doxcom <aldoperez1319@gmail.com>
2024-09-03 17:51:48 -0600

    implemented Map impl for Visit Service closes #46

---
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/doxcom/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package doxcom.springframework.sfgpetclinic.services;
+
+import doxcom.springframework.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
