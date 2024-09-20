48a087e8ababaf7ff03f4f382048d83a08285732
doxcom <aldoperez1319@gmail.com>
2024-09-20 00:57:44 -0600

    upgraded to Junit 5. Closes #4

--- sfg-pet-clinic/pet-clinic-web/src/test/java/doxcom/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ sfg-pet-clinic/pet-clinic-web/src/test/java/doxcom/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,13 +1,18 @@
 package doxcom.springframework.sfgpetclinic;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+@ExtendWith(SpringExtension.class) //junit5 syntax
 @SpringBootTest
 class SfgPetClinicApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
