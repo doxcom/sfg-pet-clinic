3f4809f2bf8b2fce468bbecaa2803ee2f7ac5d6a
doxcom <aldoperez1319@gmail.com>
2024-09-25 19:06:14 -0600

    adding mvc testing, need to check standalonesetup bug , lib is not found

---
+++ sfg-pet-clinic/pet-clinic-web/src/test/java/doxcom/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -0,0 +1,55 @@
+package doxcom.springframework.sfgpetclinic.controllers;
+
+import doxcom.springframework.sfgpetclinic.model.Owner;
+import doxcom.springframework.sfgpetclinic.services.OwnerService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+class OwnerControllerTest {
+
+    @Mock
+    OwnerService ownerService;
+
+    @InjectMocks
+    OwnerController controller;
+
+    Set<Owner> owners;
+    //we going to create inside setup instead
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        owners = new HashSet<>();
+        owners.add(Owner.builder().id(1L).build());
+        owners.add(Owner.builder().id(2L).build());
+
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
+    @Test
+    void listOwners() throws Exception {
+        when(ownerService.findAll()).thenReturn(owners);
+        mockMvc.perform(get("/owners")).andExpect(status().is(200));
+    }
+
+    @Test
+    void findOwner() {
+    }
+}
