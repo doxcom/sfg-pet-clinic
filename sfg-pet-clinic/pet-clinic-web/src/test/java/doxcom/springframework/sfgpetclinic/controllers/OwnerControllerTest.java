d3c95e5e58f3378384544f81c130c28bcffa1574
doxcom <aldoperez1319@gmail.com>
2024-09-26 16:48:31 -0600

    test owners added, pending to solve due to dependencies

--- sfg-pet-clinic/pet-clinic-web/src/test/java/doxcom/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
+++ sfg-pet-clinic/pet-clinic-web/src/test/java/doxcom/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
@@ -15,10 +16,12 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.util.HashSet;
 import java.util.Set;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
 class OwnerControllerTest {
@@ -43,13 +46,31 @@ class OwnerControllerTest {
         mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
     }
 
-    @Test
+    @Test //not running due to pom dependencies
     void listOwners() throws Exception {
         when(ownerService.findAll()).thenReturn(owners);
-        mockMvc.perform(get("/owners")).andExpect(status().is(200));
+        mockMvc.perform(get("/owners"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/index"))
+                .andExpect(model().attribute("owners", hasSize(2)));
     }
 
-    @Test
-    void findOwner() {
+    @Test //not running due to pom dependencies
+    void listOwnersByIndex() throws Exception {
+        when(ownerService.findAll()).thenReturn(owners);
+        mockMvc.perform(get("/owners/index"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/index"))
+                .andExpect(model().attribute("owners", hasSize(2)));
+    }
+
+
+    @Test //not running due to pom dependencies
+    void findOwner() throws Exception {
+        mockMvc.perform(get("owners/find"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("notimplemented"));
+
+        verifyNoInteractions(ownerService);
     }
 }
