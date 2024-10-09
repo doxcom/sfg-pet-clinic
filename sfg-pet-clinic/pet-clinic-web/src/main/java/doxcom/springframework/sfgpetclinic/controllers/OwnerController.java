da8a27e78a2a8ecb802cbd776489a75285e79f5f
doxcom <aldoperez1319@gmail.com>
2024-10-08 21:23:18 -0600

    modifiying owner view

--- sfg-pet-clinic/pet-clinic-web/src/main/java/doxcom/springframework/sfgpetclinic/controllers/OwnerController.java
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/doxcom/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -2,8 +2,11 @@ package doxcom.springframework.sfgpetclinic.controllers;
 
 import doxcom.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @RequestMapping("/owners")
@@ -25,5 +28,13 @@ public class OwnerController {
     @RequestMapping("/find")
     public String findOwner(Model model) {
         return "notImplemented";
+    }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId){
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(ownerId));
+
+        return mav;
     }
 }
